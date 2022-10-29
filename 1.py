b88f50f66768d5a131dbad4cd28a23d373600fa6
JulianneSu <U_sub@mail.ru>
2022-10-29 13:30:35 +0300

    ``

---
+++ 1.py
@@ -0,0 +1,9 @@
+#1-4
+a1 = [i for i in range(1,51)]
+#print(a1)
+a2 = [i for i in range (50, 0, -1)]
+#print(a2)
+a3 = [i**2 for i in range (1, 12)]
+a4 = [i**2 for i in range (11, 0, -1)]
+a5 = [i for i in range(2,41, 2)]
+print(a4)
---
+++ 2.py
@@ -0,0 +1,12 @@
+#6-9
+a6 = [i**2 for i in range(1, 11)]
+q = 1
+for i in range( len(a6)):
+    a6[i] *= q
+    q *= (-1)
+
+
+a7 = []
+while
+
+print(a6)
