3aa5ee9b0d237af1097102cc6ef572968eb4765b
JulianneSu <U_sub@mail.ru>
2022-10-29 14:18:43 +0300

    ``

--- 2.py
+++ 2.py
@@ -1,12 +1,15 @@
 #6-9
-a6 = [i**2 for i in range(1, 11)]
-q = 1
-for i in range( len(a6)):
-    a6[i] *= q
-    q *= (-1)
-
+a6 = [i**2 if i%2!=0  else i**2*(-1) for i in range(1,11) ]
 
 a7 = []
-while
 
-print(a6)
+a7=[2]
+for i in range (1, 11):
+    a7.append(i+ a7[i-1])
+
+
+a8=[i if i%2!=0 else i*3 for i in range(1,12)]
+
+a9=[]
+
+print(a8)
