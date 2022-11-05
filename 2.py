3309de33774c849caf51b276c9a57473ea6c3a78
Julianne <U_sub@mail.ru>
2022-11-05 16:37:49 +0300

    a7

--- 2.py
+++ 2.py
@@ -1,9 +1,10 @@
 #6-9
 a6 = [i**2 if i%2!=0  else i**2*(-1) for i in range(1,11) ]
 
-a7 = []
+a7=[]
+a7 = [2 if i==0 else i+a7[i-1] for i in range(11)]
 
-a7=[2]
+
 for i in range (1, 11):
     a7.append(i+ a7[i-1])
 
@@ -12,4 +13,4 @@ a8=[i if i%2!=0 else i*3 for i in range(1,12)]
 
 a9=[]
 
-print(a8)
+print(a7)
